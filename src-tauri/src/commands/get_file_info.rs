1a2eb661e741d29d917e5c3dbd71e9ab066d309b
Peng <2567810155@qq.com>
2025-08-15 18:39:03 +0800

    fix: 修复拖拽启动项包含中文路径错误

--- src-tauri/src/commands/get_file_info.rs
+++ src-tauri/src/commands/get_file_info.rs
@@ -1,7 +1,8 @@
 use crate::models::file_info::FileInfo;
+use encoding_rs::GBK;
 use lnk_parser::LNKParser;
-use std::fs::{self};
-use std::path::{Path, PathBuf};
+use std::fs;
+use std::path::Path;
 use windows_icons::get_icon_base64_by_path;
 
 // 获取文件名/目录名
@@ -19,90 +20,37 @@ fn get_name(p: &Path) -> String {
     }
 }
 
-// 解析 lnk 文件 并获取真正的路径
-fn get_lnk_real_path(path: &String) -> Option<PathBuf> {
-    let info = LNKParser::from_path(path).unwrap();
-
-    let mut relative_path = "";
-    if let Some(data) = info.get_relative_path() {
-        relative_path = data.string.as_str();
-    }
-
-    let mut work_dir = "";
-    if let Some(data) = info.get_working_dir() {
-        work_dir = data.string.as_str();
-    }
-    log::info!("-----------------------------------------------------------------------");
-    log::info!("path {}", path);
-    log::info!("relative_path {}", relative_path);
-    log::info!("work_dir {}", work_dir);
-    log::info!(
-        "get_icon_location {}",
-        info.get_icon_location().as_ref().unwrap()
-    );
-    log::info!(
-        "get_name_string {}",
-        info.get_name_string().as_ref().unwrap()
-    );
-    log::info!(
-        "get_lnk_file_metadata {}",
-        info.get_lnk_file_metadata()
-            .as_ref()
-            .unwrap()
-            .get_full_path()
-    );
-    log::info!(
-        "get_name_string {}",
-        info.get_name_string().as_ref().unwrap()
-    );
-    log::info!(
-        "get_name_string {}",
-        info.get_name_string().as_ref().unwrap()
-    );
-
-    // 从 data_with_file 提取最后的文件名（OsStr）
-    let file_name = Path::new(relative_path).file_name()?;
-    // 将 base 转为 PathBuf 并 push 文件名（会自动处理分隔符）
-    let mut full = PathBuf::from(work_dir);
-    full.push(file_name);
-    // if (work_dir.is_empty()) {
-    //     if let Some(data) = info.get_link_info() {
-    //         Some(&data.local_base_path)
-    //     }
-    // }
-    dbg!(&full);
-    Some(full)
-}
-
 #[tauri::command]
 pub fn get_file_info(path: String) -> Result<FileInfo, String> {
     let p = Path::new(&path);
     let metadata = fs::metadata(&p).map_err(|e| e.to_string())?;
 
-    let full_path: String;
+    let mut full_path: String;
     // 检查路径是否是 lnk 文件
     if p.extension().map_or(false, |ext| ext == "lnk") {
+        log::info!("if");
         // 解析 lnk 文件 并获取真正的路径
-        // let lnk_info = LNKParser::from_path(&path).unwrap();
+        let lnk_info = LNKParser::from_path(&path).unwrap();
+        full_path = lnk_info
+            .get_target_full_path()
+            .as_deref()
+            .unwrap()
+            .to_string();
 
-        match get_lnk_real_path(&path) {
-            Some(f_path) => full_path = f_path.to_string_lossy().into_owned(),
-            None => full_path = "".to_string(),
-        }
-
-        log::info!("full_path {}", full_path);
-
-        // if let Some(f_path) = get_lnk_real_path(&path) {
-        //     full_path = f_path.to_string_lossy().into_owned();
-        // } else {
-        //     full_path = "".to_string();
-        // }
+        // 解析出来的带中文的路径会存在乱码 转换为正常的内容
+        let raw_bytes: Vec<u8> = full_path.chars().map(|c| c as u8).collect();
+        let (fixed, _, _) = GBK.decode(&raw_bytes);
+        full_path = fixed.into_owned();
     } else {
+        log::info!("else");
         full_path = p.to_str().unwrap_or_default().to_string();
     }
 
+    log::info!("full_path --->{}", full_path);
+
     // 获取文件或目录名称
     let name = get_name(p);
+
     let base64 = get_icon_base64_by_path(&full_path).unwrap();
     let icon = "data:image/png;base64,".to_string() + &base64;
 
